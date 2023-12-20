272132f0572c1bd9eaa6b7f9c141358ce52cdc30
keerthanamunusamy05 <133357410+keerthanamunusamy05@users.noreply.github.com>
2023-12-20 15:44:50 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,10 @@
-
-#new pull
+# app.py
+# This is a test commit
 def add(a, b):
-  return a + b
-  
-def test_add()
-assert add(1, 2) == 3
-assert add(1, -1) == 0
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+
   
